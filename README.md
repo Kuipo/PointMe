c3f794efcbfa96cffc4e80c7db04c011b9a524b9
Nic <kuipo@pak0.com>
2024-02-24 20:17:50 -0800

    Inital commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # PointMe
-PointMe is a World of Warcraft plugin that allows you to "sync" map pins with party members automatically.
+PointMe is a World of Warcraft plugin that allows you to "sync" map pins with party members automatically. If users in the same party have the plugin installed, any map pin placed will place the same pin on all other party member's maps.
+
+# Features
+- Map pins sync to party members automatically
+- Map pins auto-track when placed
+
+# Todo List
+- Remove all the extra print statements
+- Change "GUILD" to "PARTY"
+- Fix issue with it sending an extra message back when you set a pin (currently it sends it, and then recieves it back once)
+- Remove AceConsole-3.0 lib files, embed in the embeds.xml, and the PointMe:SlashProcessorFunc and PointMe:RegisterChatCommand stuff
+- Put it on gitlab
+- Upload it to curse or similar?
+- Check if in a party in the OnMapPinChanged func to skip everything if we're not in a party at all
+- Fix bug: setting map pins quickly caused it to loop and set a bunch of pins 
+- Enhancement: Sync removing pins too?
+- Add multiple pins and allow syncing them all (potentially color coding them to the class color?) - https://www.curseforge.com/wow/addons/mappinenhanced
---
+++ core.lua
@@ -0,0 +1,75 @@
+local ADDON_NAME = "PointMe"
+local COMMS_PREFIX = "PointMeEvents"
+
+--- @class PointMe
+local PointMe = {}
+
+PointMe = LibStub("AceAddon-3.0"):NewAddon(ADDON_NAME, "AceEvent-3.0", "AceComm-3.0", "AceSerializer-3.0")
+
+function PointMe:OnInitialize()
+    self:RegisterEvents()
+    self:RegisterCommunication()
+end
+
+function PointMe:RegisterCommunication()
+    self:RegisterComm(COMMS_PREFIX, "CommReceived");
+end
+
+--- Processes comm messages recieved from AceComm
+-- @param prefix String classification of the message, COMMS_PREFIX
+-- @param message Data recieved, serialized
+-- @param distribution Addon channel, "PARTY"
+-- @param sender Character name of the user that sent the message
+function PointMe:CommReceived(prefix, message, distribution, sender)
+    if sender ~= UnitName("player") then
+        print("  Message from other");
+        local success, PinData = self:Deserialize(message);
+        self:SetMapPin(PinData);
+    else
+        print("  Message from self, skipping");
+    end
+end
+
+function PointMe:RegisterEvents()
+    self:RegisterEvent("USER_WAYPOINT_UPDATED", "OnMapPinChanged");
+end
+
+--- Sets a map pin at a specific point
+-- @param newPinData UiMapPoint object to set
+function PointMe:SetMapPin(newPinData)
+    if C_Map.HasUserWaypoint() == true then
+        -- print('User has pin, checking it');
+        local curPinData = C_Map.GetUserWaypoint();
+        if curPinData.uiMapID == newPinData.uiMapID and curPinData.position.x == newPinData.position.x and curPinData.position.y == newPinData.position.y then
+            -- print('New pin and old pin match, stopping.');
+        else
+            -- print('Setting new pin');
+            C_Map.SetUserWaypoint(newPinData);
+        end
+    else
+        -- print('No pin detected, setting new pin');
+        C_Map.SetUserWaypoint(newPinData);
+    end
+end
+
+--- Used to autotrack the waypoint after being set
+function PointMe:TrackWaypoint()
+    C_SuperTrack.SetSuperTrackedUserWaypoint(true);
+end
+
+function PointMe:SendPinToComms()
+    local pinData = C_Map.GetUserWaypoint();
+    local serializedPinData = self:Serialize(pinData);
+    self:SendCommMessage(COMMS_PREFIX, serializedPinData, "PARTY");
+end
+
+function PointMe:OnMapPinChanged()
+    if C_Map.HasUserWaypoint() == true then
+        RunNextFrame(function() self:TrackWaypoint() end);
+        self:SendPinToComms();
+    end
+end
+
+PointMe:OnInitialize()
+
+return PointMe
